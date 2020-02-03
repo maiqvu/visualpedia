1e4c9ff484619b20b12199657c294a530c27b634
mai <58199372+maiqvu@users.noreply.github.com>
2020-02-03 14:56:27 +1100

    Updates

---
+++ precache-manifest.2c0fb37a4d1faa362b915c57891c5c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7904efd5e293d0f84456f24d45a1ee9a",
+    "url": "/visualpedia/index.html"
+  },
+  {
+    "revision": "29b7724f9ca74deb5a51",
+    "url": "/visualpedia/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "59ebe0b9cead67648d90",
+    "url": "/visualpedia/static/css/main.96f41c56.chunk.css"
+  },
+  {
+    "revision": "29b7724f9ca74deb5a51",
+    "url": "/visualpedia/static/js/2.874f735f.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/visualpedia/static/js/2.874f735f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ebe0b9cead67648d90",
+    "url": "/visualpedia/static/js/main.acda9c70.chunk.js"
+  },
+  {
+    "revision": "2325f206c5b36f1f0a4c",
+    "url": "/visualpedia/static/js/runtime-main.3acc8df6.js"
+  }
+]);
