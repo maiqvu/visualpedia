556b5cfa4d7d8db80504f42a37422b24a2ebffab
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-05 14:58:02 +1100

    need to pull

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import '../NewsFeed.css'
-const URL = 'https://newsapi.org/v2/top-headlines?country='
-
-const API_KEY = 'apiKey=fc488d7d92e0499888abe5ddb0f1b9d8'
-
+const natgeo = require('national-geographic-api').NationalGeographicAPI;
 class NewsFeed extends React.Component {
 
   state = {
-    allArticles: []
+    allArticles: [],
   }
-  performSearch = (search='us') => {
-    axios.get(`${URL}${search}&${API_KEY}`)
-    .then(res => {
-      console.log(res.data.articles);
-      this.setState({allArticles: res.data.articles})
-    })
-    .catch( err => {
-      console.warn(err)
-    })
+
+
+  performSearch = () => {
+    natgeo.getLatestNews()
+    .then(result => {console.log(result)})
+    .catch(err => console.warn(err))
   }
 
   limitString = (longString) => {
@@ -27,30 +21,33 @@ class NewsFeed extends React.Component {
   }
     componentDidMount(){
 
-       this.performSearch(this.props.localSearch)
+       this.performSearch()
     }
 
     render(){
+
       return(
-        <div className="articles">
-          <h5 className="newsHeadline">Local News</h5>
-          {
-            this.state.allArticles !== 0
-            ?
-            <div className="artilceList">
-            {this.state.allArticles.map((t, index) => {
-              return (
-              <div key={index} className="artilceListDiv">
-                    <a href={t.url} className="newsFeed"
-                    target="_blank"><span>{this.limitString(t.title)}...</span></a>
-              </div>
-              )
-            })}
-            </div>
-            :
-            <h1></h1>
-          }
-        </div>
+
+        <h1>test</h1>
+        // <div className="articles">
+        //   <h5 className="newsHeadline">Local News</h5>
+        //   {
+        //     this.state.allArticles !== 0
+        //     ?
+        //     <div className="artilceList">
+        //     {this.state.allArticles.map((t, index) => {
+        //       return (
+        //       <div key={index} className="artilceListDiv">
+        //             <a href={t.url} className="newsFeed"
+        //             target="_blank"><span>{this.limitString(t.title)}...</span></a>
+        //       </div>
+        //       )
+        //     })}
+        //     </div>
+        //     :
+        //     <h1></h1>
+        //   }
+        // </div>
       )
     }
 }
---
+++ src/components/NewsFeedOne.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import '../NewsFeed.css'
+const URL = 'https://newsapi.org/v2/top-headlines?country='
+
+const API_KEY = 'apiKey=fc488d7d92e0499888abe5ddb0f1b9d8'
+
+class NewsFeed extends React.Component {
+
+  state = {
+    allArticles: []
+  }
+  performSearch = (search='us') => {
+    axios.get(`${URL}${search}&${API_KEY}`)
+    .then(res => {
+      console.log(res.data.articles);
+      this.setState({allArticles: res.data.articles})
+    })
+    .catch( err => {
+      console.warn(err)
+    })
+  }
+
+  limitString = (longString) => {
+      longString = longString.substring(0,50)
+    return longString
+  }
+    componentDidMount(){
+
+       this.performSearch(this.props.localSearch)
+    }
+
+    render(){
+      return(
+        <div className="articles">
+          <h5 className="newsHeadline">Local News</h5>
+          {
+            this.state.allArticles !== 0
+            ?
+            <div className="artilceList">
+            {this.state.allArticles.map((t, index) => {
+              return (
+              <div key={index} className="artilceListDiv">
+                    <a href={t.url} className="newsFeed"
+                    target="_blank"><span>{this.limitString(t.title)}...</span></a>
+              </div>
+              )
+            })}
+            </div>
+            :
+            <h1></h1>
+          }
+        </div>
+      )
+    }
+}
+
+export default NewsFeed
