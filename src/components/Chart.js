f9d399c5720e844c4a5e5cade2f1f23aab9d2057
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-05 14:20:54 +1100

    fixed color bug

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,33 +2,16 @@ import React from 'react'
 import {Bar, Line, Radar, HorizontalBar} from 'react-chartjs-2';
 import _ from 'lodash/collection';
 import randomColor from 'randomcolor';
-
+let counter
 const colorWheel = [
       'rgba(255, 99, 132, 1)',
       'rgba(54, 162, 235, 1)',
       'rgba(255, 206, 86, 1)',
       'rgba(75, 192, 192, 1)',
       'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)'
 ];
 const chartStyle = () => ({
-  // fill: false,
-  // lineTension: 0.1,
-  // borderColor: randomColor(),
-  //backgroundColor: randomColor(),
-  // borderCapStyle: 'butt',
-  // borderDash: [],
-  // borderDashOffset: 0.0,
-  // borderJoinStyle: 'miter',
-  // pointBorderColor: randomColor(),
-  // pointBackgroundColor: randomColor(),
-  // pointBorderWidth: 1,
-  // pointHoverRadius: 5,
-  // pointHoverBackgroundColor: randomColor(),
-  // pointHoverBorderColor: randomColor(),
-  // pointHoverBorderWidth: 2,
-  // pointRadius: 1,
-  // pointHitRadius: 10,
+
 });
 
 class Chart extends React.Component {
@@ -53,19 +36,24 @@ class Chart extends React.Component {
     for (const country in resultsToSort) {
       // sort counrties by order of dates
       let countries = _.sortBy(resultsToSort[country], ['date'])
-      const bg = {backgroundColor: colorWheel[counter]}
-      counter++;
+      // const bg = {backgroundColor: colorWheel[counter]}
       // push new
+      console.log('new counter', counter);
       datasets.push({
         // side datasets chart js need label and data
         label: country,
         // map through data give the value and spread the chartStyle
-        data: countries.map(v => v.value), ...bg // ...chartStyle(),
+        data: countries.map(v => v.value),
+
+        backgroundColor: colorWheel[counter]
+         // ...chartStyle(),
       });// push
+      counter++;
     } // for
     // state of data is being changed therefore inner data can replace*
-    this.setState({data: {datasets: datasets}});  // ...this.state.data,
-
+    this.setState({data: {datasets: datasets } } );
+    // () => console.log('STATE', this.state) );  // ...this.state.data,
+    console.log(datasets);
   }
 
   componentDidMount(){
