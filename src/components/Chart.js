7b40d9c323adab56cdcd41dfff7c1e1c6444b922
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-02-05 16:10:18 +1100

    chart options & gradient

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -26,7 +26,7 @@ class Chart extends React.Component {
     displayTitle: true,
     displayLegend: true,
     legendPosition: 'right',
-    
+
   }
 
   updateDataSets(resultsToSort) {
@@ -67,6 +67,68 @@ class Chart extends React.Component {
     }
   }
 
+  getOptionsForChart = chart => {
+    let options = {};
+    switch(chart) {
+      case 'line':
+        options = {
+          scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        }
+        };
+        break;
+
+      case 'bar':
+      case 'horziontal':
+        options = {
+
+        };
+        break;
+      case 'radar':
+        options = {
+
+        };
+        break;
+    }
+
+    return options;
+  }
+
+  setGradientColor = (canvas, color, colorStop) => {
+    const ctx = canvas.getContext('2d');
+    const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(0, color);
+    gradient.addColorStop(0.95, colorStop);
+    return gradient;
+  }
+
+  getChartData = canvas => {
+    const data = this.state.data;
+    if (data.datasets) {
+      const colors = ['rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)'
+      ];
+      const colorStops = ['rgba(255, 159, 192, 1)',
+                          'rgba(114, 222, 255, 1)',
+                          'rgba(255, 246, 146, 1)',
+                          'rgba(135, 242, 252, 1)',
+                          'rgba(213, 152, 255, 1)'
+      ];
+      data.datasets.forEach((set, i) => {
+        set.backgroundColor = this.setGradientColor(canvas, colors[i], colorStops[i]);
+        // set.borderColor = "black";
+        // set.borderWidth = 1;
+      });
+    }
+
+    return data;
+  }
+
 
 
   // addColors = () => {
@@ -100,25 +162,28 @@ class Chart extends React.Component {
         radar: Radar
       };
 
+    const generalGlobalOptions = {
+      maintainAspectRatio: true,
+      title: {
+        display: true,
+        text: this.props.title,
+        fontSize: 25
+      },
+      legend: {
+        display: this.props.displayLegend,
+        position: this.props.legendPosition,
+        labels: {
+          display: true
+        }
+      }
+    }
+
+
     const ChartTag = components[this.props.chart];
 
     return(
       <div className="Chart">
-        <ChartTag data={this.state.data} options={{ //this.props.data
-        maintainAspectRatio: true,
-        title: {
-          display: true,
-          text: this.props.title,
-          fontSize: 25
-        },
-        legend: {
-          display: this.props.displayLegend,
-          position: this.props.legendPosition,
-          labels: {
-            display: true
-          }
-        }
-        }}/>
+        <ChartTag data={this.getChartData} options={{...generalGlobalOptions, ...this.getOptionsForChart(this.props.chart)}}/>
       </div>
     ) // return
   } //render
