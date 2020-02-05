a854308e2f757b2393beb9a6634636266d78abb0
Zhuang Xu <not-real@example.com>
2020-02-06 10:30:53 +1100

    Updates

--- precache-manifest.18cf04ebb722a1bec0ed35c225a1c5de.js
+++ precache-manifest.18cf04ebb722a1bec0ed35c225a1c5de.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48476aa5755d3cb14980853985369c9e",
+    "revision": "7b7dbfb803b69e39d1bed1387b84a2b6",
     "url": "/visualpedia/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visualpedia/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "427d3ef973e2cfa91238",
+    "revision": "90ae7b32fd26b7d9c9ef",
     "url": "/visualpedia/static/css/main.4adfa2db.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/visualpedia/static/js/2.c6639a0e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "427d3ef973e2cfa91238",
-    "url": "/visualpedia/static/js/main.f505de6e.chunk.js"
+    "revision": "90ae7b32fd26b7d9c9ef",
+    "url": "/visualpedia/static/js/main.89233418.chunk.js"
   },
   {
     "revision": "2325f206c5b36f1f0a4c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visualpedia/precache-manifest.88a46bb1ef4f0fcaa1ecd2aa36b00847.js"
+  "/visualpedia/precache-manifest.18cf04ebb722a1bec0ed35c225a1c5de.js"
 );
 
 self.addEventListener('message', (event) => {
