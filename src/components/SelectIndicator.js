85b2a3e1b67c163b754959e30b703dd9145abdc4
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-02-06 16:20:59 +1100

    search filter with clickhandler

--- src/components/SelectIndicator.js
+++ src/components/SelectIndicator.js
@@ -8,19 +8,13 @@ function SelectIndicator(props){
       props.countriesLabels.length !== 0
       ?
       <div>
-      {/*<select onChange={props.handleChange}>
-        <option value="AG.LND.AGRI.ZS">Agricultural land (%)</option>
-        <option value="EG.FEC.RNEW.ZS">Renewable Consupstion</option>
-        <option value="NY.GDP.TOTL.RT.ZS">Natural Resource Rents</option>
-        <option value="NY.GDP.MKTP.CD">Gross Domestic Product</option>
-      </select>*/}
       <form onSubmit={props.handleSubmit}>
         <input type="text" onChange={props.handleChange} />
         <input type="submit" value="Search" />
       </form>
       <ul>
         {props.labels.map((res, ind) => {
-          return <li key={ind}>{res}</li>;
+          return <li key={ind} onClick={props.handleSubmit} data-indicator={res[1]}>{res[0]}</li>;
         })}
       </ul>
       </div>
