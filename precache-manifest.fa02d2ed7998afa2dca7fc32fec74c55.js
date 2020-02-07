96460619b046371694166cc776170ef617be8866
Zhuang Xu <not-real@example.com>
2020-02-07 14:30:45 +1100

    Updates

---
+++ precache-manifest.fa02d2ed7998afa2dca7fc32fec74c55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a688296bfee9149984327bb26c12bc40",
+    "url": "/visualpedia/index.html"
+  },
+  {
+    "revision": "61f50b31d9b725368117",
+    "url": "/visualpedia/static/css/2.a255583a.chunk.css"
+  },
+  {
+    "revision": "7d1d985fa713f85c68a3",
+    "url": "/visualpedia/static/css/main.c594142b.chunk.css"
+  },
+  {
+    "revision": "61f50b31d9b725368117",
+    "url": "/visualpedia/static/js/2.e6478e41.chunk.js"
+  },
+  {
+    "revision": "6e6198b0838f1b33cf5e66d002284e73",
+    "url": "/visualpedia/static/js/2.e6478e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1d985fa713f85c68a3",
+    "url": "/visualpedia/static/js/main.dd518340.chunk.js"
+  },
+  {
+    "revision": "2325f206c5b36f1f0a4c",
+    "url": "/visualpedia/static/js/runtime-main.3acc8df6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visualpedia/precache-manifest.ee337168dcd48e12a78545b925dfe9e6.js"
+  "/visualpedia/precache-manifest.fa02d2ed7998afa2dca7fc32fec74c55.js"
 );
 
 self.addEventListener('message', (event) => {
